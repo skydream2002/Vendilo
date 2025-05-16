02ab12fb29d9ad2d820abab8d9bf9c1f4cecdb94
mohammadreza <mohamadreza.akbari2002@gmail.com>
2025-05-16 16:23:12 +0330

    creat getAverageRating method with lambda expression and some getter, setter

--- src/main/java/ir/ac/kntu/Product.java
+++ src/main/java/ir/ac/kntu/Product.java
@@ -1,6 +1,7 @@
 package ir.ac.kntu;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public abstract class Product {
     private String name;
@@ -8,6 +9,16 @@ public abstract class Product {
     private ProductType type;
     private Seller seller;
     private List<Double> ratings = new ArrayList<>(); 
+
+    public abstract void showDetails();
+
+    public String getAverageRating() {
+        if (ratings == null || ratings.isEmpty()) {
+            return "-";
+        }
+        OptionalDouble averageRates = ratings.stream().mapToDouble(Double::doubleValue).average();
+        return averageRates.isPresent() ? (String.format("%.2f",averageRates.getAsDouble())) : "-";
+    }
     
     public String getName() {
         return name;
@@ -26,5 +37,21 @@ public abstract class Product {
     }
     public void setType(ProductType type) {
         this.type = type;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public List<Double> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Double> ratings) {
+        this.ratings = ratings;
     }
 }
